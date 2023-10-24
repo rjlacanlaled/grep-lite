60cdbf1b99885502669bf9b893eb9be234fb1f90
rlacanlale@ubx.ph <rlacanlale@ubx.ph>
2023-10-24 14:01:03 +0800

    implement main logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{env, fs::File, io::BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let search_term = &args[1];
+    let path = &args[2];
+    let file = File::open(path).expect("Could not open file");
+    let reader = std::io::BufReader::new(file);
+
+    for line in reader.lines(){
+        if line.is_err() {
+            continue;
+        }
+        let content = line.unwrap();
+        if content.contains(search_term) {
+            println!("{}", content);
+        }
+    }
 }
